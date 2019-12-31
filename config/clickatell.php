f0bd8205337f436d400cfc127ef5656ff228023c
Naftal Timotheus <tnaffh@gmail.com>
2019-12-31 11:36:10 +0200

    restructure

---
+++ config/clickatell.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+'api_key'=> env('CLICKATELL_API_KEY')
+];
--- src/Clickatell.php
+++ src/Clickatell.php
@@ -1,58 +1,57 @@
 <?php
 
-namespace Ccom\Clickatell;
+
+namespace Tnaffh\Clickatell;
+
 
 use GuzzleHttp\Client as Guzzle;
 
 class Clickatell
 {
-	protected $client;
+    protected $rest;
+    protected $endpoint = 'https://platform.clickatell.com/messages';
 
-	protected $endpoint = 'https://platform.clickatell.com/messages';
+    public function __construct()
+    {
+        $this->rest = new Guzzle($this->getHeaders());
+    }
 
-	public function __construct()
-	{
-		$this->client = new Guzzle($this->getHeaders());
-	}
+    public function sendMessage(string $to, $message)
+    {
+        try {
+            return $this->rest->post($this->endpoint, [
+                'json' => [
+                    'content' => $message,
+                    "to" => [$to]
+                ]
+            ]);
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage(), 422);
+        }
+    }
+    public function sendMessages(array $to, $message)
+    {
+        if(count($to) > 500)
+            throw new \Exception("Max Limit Reached, can only send to 500 in a single request, " . count($to) . " given.", 422);
+        try {
+            return $this->rest->post($this->endpoint, [
+                'json' => [
+                    'content' => $message,
+                    "to" => $to
+                ]
+            ]);
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage(), 422);
+        }
+    }
 
-	public function sendMessage(string $to, $message)
-	{
-		try {
-			return $this->client->post($this->endpoint, [
-				'json' => [
-					'content' => $message,
-					"to" => [$to]
-				]
-			]);
-		} catch (\Exception $e) {
-			throw new \Exception($e->getMessage(), 422);
-		}
-	}
+    protected function getHeaders()
+    {
+        return [
+            'headers' => [
+                'Authorization' => config('clickatell.api_key')
+            ]
+        ];
+    }
 
-	public function sendMessages(array $to, $message)
-	{
-		if(count($to) > 500)
-			throw new \Exception("Max Limit Reached, can only send to 500 in a single request, " . count($to) . " given.", 422);
-
-		try {
-			return $this->client->post($this->endpoint, [
-				'json' => [
-					'content' => $message,
-					"to" => $to
-				]
-			]);
-		} catch (\Exception $e) {
-			throw new \Exception($e->getMessage(), 422);
-		}
-	}
-
-	protected function getHeaders()
-	{
-		return [
-			'headers' => [
-				'Authorization' => config('clickatell.api_key')
-			]
-		];
-	}
-	
 }
